0ca3c1b1819cf2dcd4c98c10fe19cc7e81ebb5ef
Raymond <raymond007mg@gmail.com>
2026-09-15 22:27:06 -0500

    agregar ejercicio 3

---
+++ Ejercicio3.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ejercicio3 {
+
+    // Método principal
+    public static void main(String[] args) {
+
+        Scanner teclado = new Scanner(System.in);
+
+        System.out.print("Ingrese la cantidad de metros: ");
+        double metros = teclado.nextDouble();
+
+        double pies = metrosAPies(metros);
+        double pulgadas = metrosAPulgadas(metros);
+        double centimetros = metrosACentimetros(metros);
+
+        System.out.println("Luego de las conversiones los valores son:");
+        System.out.println("Metros: " + metros);
+        System.out.println("Pies: " + pies);
+        System.out.println("Pulgadas: " + pulgadas);
+        System.out.println("Centímetros: " + centimetros);
+
+        teclado.close();
+    }
+
+    // Convierte metros a pies
+    public static double metrosAPies(double metros) {
+        return metros * 3.28084;
+    }
+
+    // Convierte metros a pulgadas
+    public static double metrosAPulgadas(double metros) {
+        return metros * 39.3701;
+    }
+
+    // Convierte metros a centímetros
+    public static double metrosACentimetros(double metros) {
+        return metros * 100;
+    }
+}
